e94330e8fb7dc22af14332a8a32b772bea1526f1
bleachman <bleachman@live.com.pt>
2023-12-02 16:09:20 +0000

    d2 completed

---
+++ d2.py
@@ -0,0 +1,34 @@
+import re
+
+with open("d2input.txt") as f:
+    lineFiles = f.readlines()
+
+
+possibleGames: list[int] = []
+MaxColor = {"red": 12, "blue": 14, "green": 13}
+s = 0
+
+for line in lineFiles:
+    line = line.replace("\n", "")
+    gameSplit = line.split(":")
+    game = int(gameSplit[0].split(" ")[1])
+    colorDict: dict[str, int] = {}
+    # get list of max value per color
+    for col in re.split(";|,", gameSplit[1]):
+        colorPair = col.split(" ")
+        colorDict[colorPair[2]] = max(
+            int(colorDict.get(colorPair[2], 0)), int(colorPair[1])
+        )
+    # get possible games for part 1
+    possible = True
+    for colour in ["red", "blue", "green"]:
+        if MaxColor[colour] < colorDict[colour]:
+            possible = False
+    if possible:
+        possibleGames.append(game)
+
+    # sum the  multiplication for part 2
+    s += colorDict["red"] * colorDict["blue"] * colorDict["green"]
+print(f"Possible games list -> {possibleGames}")
+print(f"Result of part1 -> {sum(possibleGames)}")
+print(f"Result of part2 -> {s}")
