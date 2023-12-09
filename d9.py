12636f4c5cd0b5814d44c9bfd6380e45cbab94ca
bleachman <bleachman@live.com.pt>
2023-12-09 14:44:06 +0000

    day 9 part 1

---
+++ d9.py
@@ -0,0 +1,45 @@
+with open("inputs/d9input.txt") as f:
+    lineFiles = f.readlines()
+
+
+def get_down_sequence(nums: list[int]) -> list[int]:
+    res = []
+    for i in range(len(nums) - 1):
+        res.append(nums[i + 1] - nums[i])
+    return res
+
+
+def get_up_sequence(downList: list[list[int]]) -> list[list[int]]:
+    downList[-1].append(0)
+    lastNum = 0
+    l_downList = len((downList))
+    for i, nums in enumerate(reversed(downList[:-1])):
+        lastNum = lastNum + nums[-1]
+        downList[l_downList - i - 2].append(lastNum)
+    return downList
+
+
+def get_total_down_sequence(line: list[int]) -> list[list[int]]:
+    res = [line]
+    for i in range(len(line)):
+        newLine = get_down_sequence(res[-1])
+        res.append(newLine)
+        if not any(newLine):
+            break
+
+    return res
+
+
+r = 0
+for line in lineFiles:
+    downRes = get_total_down_sequence([int(i) for i in line.split()])
+    upRes = get_up_sequence(downRes)
+    r += upRes[0][-1]
+
+print(f"Result of part 1 -> {r}")
+
+# downResTest = get_total_down_sequence([int(i) for i in lineFiles[0].split()])
+# print(downResTest)
+
+# upResTest = get_up_sequence(downResTest)
+# print(upResTest)
