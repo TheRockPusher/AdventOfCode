c7b1356c2dfec44b860f07ff3381588682ced765
bleachman <bleachman@live.com.pt>
2023-12-10 19:46:29 +0000

    d10 p2

--- d10.py
+++ d10.py
@@ -60,6 +60,32 @@ def find_cycle(lines: list[str], startPosition: Vector, startVelocity: Vector):
     return path
 
 
+def find_S_letter(path):
+    p1 = path[1] - path[0]
+    p2 = path[-1] - path[0]
+    setP = set([p1, p2])
+    for i in directions.keys():
+        if len(directions[i] - setP) == 0:
+            return i
+
+
+def enclosed_squares(path: list[Vector]):
+    path = sorted(path, key=lambda pos: (pos.y, pos.x))
+    path = path
+    crossings = ["|", "J", "L"]
+    if find_S_letter(maxRes) in crossings:
+        crossings.append("S")
+    crossNum = 0
+    enclosed_tiles = 0
+    for i, tile in enumerate(path[:-1]):
+        if lineFiles[tile.y][tile.x] in crossings:
+            crossNum += 1
+        if crossNum % 2 != 0 and tile.y == path[i + 1].y:
+            enclosed_tiles += path[i + 1].x - tile.x - 1
+
+    return enclosed_tiles
+
+
 startLocation = find_S(lineFiles)
 possibilities = [Vector(0, 1), Vector(1, 0), Vector(-1, 0), Vector(0, -1)]
 steps = 0
@@ -67,5 +93,8 @@ for sVel in possibilities:
     res = find_cycle(lineFiles, startLocation, sVel)
     if len(res) > steps:
         steps = len(res)
+        maxRes = res
         maxPosition = res[steps // 2]
+print("PART1:")
 print(f"Farthest Point -> {maxPosition}\nStep -> {steps//2}")
+print(f"PART2 ->  {enclosed_squares(maxRes)}")
