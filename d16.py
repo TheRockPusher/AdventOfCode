e1282ced4d9ea6815c78c3c2e75673e622cf4ef4
bleachman <bleachman@live.com.pt>
2023-12-16 16:12:23 +0000

    d16 part2

--- d16.py
+++ d16.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
+
 from dataclasses import dataclass, field
+from functools import reduce
 
 with open("inputs/d16input.txt") as f:
     lineFile = f.readlines()
@@ -41,26 +43,45 @@ class beam:
 
 
 mirrorMap = [line.replace("\n", "") for line in lineFile]
-startBeams = [beam(-1, 0, 1, 0)]
+startBeam = beam(-1, 0, 1, 0)
+
+
+def full_energize(mirrorMap: list[str], startBeam: beam) -> set[tuple[int, int]]:
+    max_x = len(mirrorMap[0])
+    max_y = len(mirrorMap)
+    historySet: set[tuple[int, int]] = set([])
+    pipe_coord: list[tuple[int, int]] = []
+    startBeams = [startBeam]
+    while startBeams:
+        objects_delete = []
+        for iBeam in startBeams:
+            iBeam.movement()
+            if iBeam.outside(max_x, max_y):
+                objects_delete.append(iBeam)
+            else:
+                if mirrorMap[iBeam.y][iBeam.x] != ".":
+                    childBeam = iBeam.collision(mirrorMap[iBeam.y][iBeam.x])
+                    if childBeam and (childBeam.x, childBeam.y) in pipe_coord:
+                        objects_delete.append(iBeam)
+                    elif childBeam:
+                        pipe_coord.append((childBeam.x, childBeam.y))
+                        startBeams.append(childBeam)
+        for beams in objects_delete:
+            historySet = historySet.union(set(beams.history))
+        startBeams = [beams for beams in startBeams if beams not in objects_delete]
+    return historySet
+
+
+historySetP1 = full_energize(mirrorMap, startBeam)
+print(f"result of part 1 -> {len(sorted(list(historySetP1)))-1}")
+res_p2 = 0
 max_x = len(mirrorMap[0])
 max_y = len(mirrorMap)
-historySet: set[tuple[int, int]] = set([])
-pipe_coord: list[tuple[int, int]] = []
-while startBeams:
-    objects_delete = []
-    for iBeam in startBeams:
-        iBeam.movement()
-        if iBeam.outside(max_x, max_y):
-            objects_delete.append(iBeam)
-        else:
-            if mirrorMap[iBeam.y][iBeam.x] != ".":
-                childBeam = iBeam.collision(mirrorMap[iBeam.y][iBeam.x])
-                if childBeam and (childBeam.x, childBeam.y) in pipe_coord:
-                    objects_delete.append(iBeam)
-                elif childBeam:
-                    pipe_coord.append((childBeam.x, childBeam.y))
-                    startBeams.append(childBeam)
-    for beams in objects_delete:
-        historySet = historySet.union(set(beams.history))
-    startBeams = [beams for beams in startBeams if beams not in objects_delete]
-print(f"result of part 1 -> {len(sorted(list(historySet)))-1}")
+startBeamP2 = reduce(
+    lambda x1, x2: x1 + x2,
+    [[beam(-1, i, 1, 0), beam(max_x, i, -1, 0)] for i in range(max_y)]
+    + [[beam(i, -1, 0, 1), beam(i, max_y, 0, -1)] for i in range(max_x)],
+)
+for bm in startBeamP2:
+    res_p2 = max(res_p2, len(full_energize(mirrorMap, bm)) - 1)
+print(f"result of part 2 -> {res_p2}")
