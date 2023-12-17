0b434362e17a9b4dd9231d59cd30bd0051bb0758
bleachman <bleachman@live.com.pt>
2023-12-17 17:54:50 +0000

    d17 p1 test working not exercise

--- d17.py
+++ d17.py
@@ -1,43 +1,73 @@
 from queue import PriorityQueue
+from dataclasses import dataclass
 
-with open("inputs/d17test") as f:
+with open("inputs/d17input.txt") as f:
     lineFile = f.readlines()
 
 
-def return_neighbours(path: list[str], coord: tuple[int, int]) -> list[tuple[int, int]]:
+@dataclass(frozen=True, order=True)
+class Coordinates:
+    x: int
+    y: int
+    past_x: int
+    past_y: int
+
+
+def previous_neighbour_path(
+    coord: Coordinates, movement: tuple[int, int]
+) -> Coordinates:
+    return (
+        Coordinates(coord.x + movement[0], coord.y, coord.past_x + movement[0], 0)
+        if movement[1] == 0
+        else Coordinates(coord.x, coord.y + movement[1], 0, coord.past_y + movement[1])
+    )
+
+
+def return_neighbours(
+    path: list[str], coord: Coordinates, max_straight: int
+) -> list[Coordinates]:
     return [
-        (x[0], x[1])
-        for x in [
-            (coord[0] + 1, coord[1]),
-            (coord[0] - 1, coord[1]),
-            (coord[0], coord[1] + 1),
-            (coord[0], coord[1] - 1),
+        c
+        for c in [
+            previous_neighbour_path(coord, (1, 0)),
+            previous_neighbour_path(coord, (-1, 0)),
+            previous_neighbour_path(coord, (0, -1)),
+            previous_neighbour_path(coord, (0, 1)),
         ]
-        if x[0] >= 0 and x[1] >= 0 and x[0] < len(path[0]) and x[1] < len(path)
+        if c.x >= 0
+        and c.y >= 0
+        and c.x < len(path[0])
+        and c.y < len(path)
+        and abs(c.past_x) <= max_straight
+        and abs(c.past_y) <= max_straight
     ]
 
 
-def pathfinding(path: list[str]):
-    frontier: PriorityQueue[tuple[int, tuple[int, int]]] = PriorityQueue()
-    frontier.put((0, (0, 0)))
-    came_from: dict[tuple[int, int], tuple[int, int]] = {(0, 0): (0, 0)}
-    cumulative_cost: dict[tuple[int, int], int] = {(0, 0): 0}
+def pathfinding(path: list[str], max_straight: int):
+    startCoordinate = Coordinates(0, 0, 0, 0)
+    frontier: PriorityQueue[tuple[int, Coordinates]] = PriorityQueue()
+    frontier.put((0, startCoordinate))
+    came_from: dict[Coordinates, Coordinates] = {startCoordinate: startCoordinate}
+    cumulative_cost: dict[Coordinates, int] = {startCoordinate: 0}
 
     while not frontier.empty():
-        current = frontier.get()
-        if current[1] == (len(path[0]) - 1, len(path) - 1):
+        _, current = frontier.get()
+        if (current.x, current.y) == (len(path[0]) - 1, len(path) - 1):
             break
-        for next in return_neighbours(path, current[1]):
-            next_cost = cumulative_cost[current[1]] + int(
-                path[current[1][1]][current[1][0]]
+        for next in return_neighbours(path, current, max_straight):
+            next_cost = cumulative_cost[current] + int(
+                path[current.y][current.x]
             )
             if next not in cumulative_cost or next_cost < cumulative_cost[next]:
                 cumulative_cost[next] = next_cost
-                came_from[next] = current[1]
+                came_from[next] = current
                 frontier.put((next_cost, next))
 
     return cumulative_cost, came_from
 
-
-a, b = pathfinding([x.replace("\n", "") for x in lineFile])
-print(a)
+path = [x.replace("\n", "") for x in lineFile]
+coordCostDict, _ = pathfinding(path,3)
+#in exercise first values doesn count but last does
+factor = int(path[-1][-1])-int(path[0][0])
+lastCoordPossibilities = [i for i in coordCostDict if i.x == len(path[0])-1 and i.y == len(path)-1]
+print(f"Result of part 1 -> {min([coordCostDict[i]+factor for i in lastCoordPossibilities])}")
