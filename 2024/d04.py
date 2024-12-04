449e8e45397797b9d017144b2adbc4ce34720c18
TheRockPusher <github@orvit.simplelogin.com>
2024-12-04 15:03:07 +0000

    d4 EX2

--- 2024/d04.py
+++ 2024/d04.py
@@ -3,31 +3,73 @@ with open("2024/inputs/d04input.txt") as f:
 
 
 # Exercise 1
-def search(lineFile,coord:tuple[int,int],direction:tuple[int,int],check_letter:int, word:str)->bool:
-    nextCoord=(coord[0]+direction[0],coord[1]+direction[1])
-    if (not 0<=nextCoord[0]<len(lineFile[0])) or (not 0<=nextCoord[1]<len(lineFile)):
+def search(
+    lineFile,
+    coord: tuple[int, int],
+    direction: tuple[int, int],
+    check_letter: int,
+    word: str,
+) -> bool:
+    nextCoord = (coord[0] + direction[0], coord[1] + direction[1])
+    if (not 0 <= nextCoord[0] < len(lineFile[0])) or (
+        not 0 <= nextCoord[1] < len(lineFile)
+    ):
         return False
-    if word[check_letter] !=lineFile[nextCoord[1]][nextCoord[0]]:
+    if word[check_letter] != lineFile[nextCoord[1]][nextCoord[0]]:
         return False
-    if check_letter==len(word)-1:
+    if check_letter == len(word) - 1:
         return True
     else:
-        return search(lineFile,nextCoord,direction,check_letter+1,word)
+        return search(lineFile, nextCoord, direction, check_letter + 1, word)
 
-def searchAllDirections(lineFile,coord:tuple[int,int],check_letter:int, word:str):
-    horizontal_search=search(lineFile,coord,(1,0),check_letter,word)
-    horizontal_opp_search=search(lineFile,coord,(-1,0),check_letter,word)
-    vertical_search=search(lineFile,coord,(0,1),check_letter,word)
-    vertical_opp_search=search(lineFile,coord,(0,-1),check_letter,word)
-    diagonal_search=search(lineFile,coord,(1,1),check_letter,word)
-    diagonal_opp_search=search(lineFile,coord,(-1,-1),check_letter,word)
-    diagonal_back_search=search(lineFile,coord,(1,-1),check_letter,word)
-    diagonal_back_opp_search=search(lineFile,coord,(-1,1),check_letter,word)
-    return sum([horizontal_search,horizontal_opp_search,vertical_search,vertical_opp_search,diagonal_search,diagonal_opp_search,diagonal_back_search,diagonal_back_opp_search])
 
-total =0
-for y,line in enumerate(lineFile):
-    for x,letter in enumerate(line):
-        if letter=='X':
-            total+=searchAllDirections(lineFile,(x,y),1,'XMAS')
+def searchAllDirections(lineFile, coord: tuple[int, int], check_letter: int, word: str):
+    horizontal_search = search(lineFile, coord, (1, 0), check_letter, word)
+    horizontal_opp_search = search(lineFile, coord, (-1, 0), check_letter, word)
+    vertical_search = search(lineFile, coord, (0, 1), check_letter, word)
+    vertical_opp_search = search(lineFile, coord, (0, -1), check_letter, word)
+    diagonal_search = search(lineFile, coord, (1, 1), check_letter, word)
+    diagonal_opp_search = search(lineFile, coord, (-1, -1), check_letter, word)
+    diagonal_back_search = search(lineFile, coord, (1, -1), check_letter, word)
+    diagonal_back_opp_search = search(lineFile, coord, (-1, 1), check_letter, word)
+    return sum(
+        [
+            horizontal_search,
+            horizontal_opp_search,
+            vertical_search,
+            vertical_opp_search,
+            diagonal_search,
+            diagonal_opp_search,
+            diagonal_back_search,
+            diagonal_back_opp_search,
+        ]
+    )
+
+
+total = 0
+for y, line in enumerate(lineFile):
+    for x, letter in enumerate(line):
+        if letter == "X":
+            total += searchAllDirections(lineFile, (x, y), 1, "XMAS")
 print(total)
+
+
+# Exercise 2
+def foundX(lineFile, coord: tuple[int, int], check_letter: int, word: str):
+    startCoordX1 = (coord[0] - 2, coord[1] - 2)
+    startCoordX2 = (coord[0] + 2, coord[1] - 2)
+    diagonal = search(lineFile, startCoordX1, (1, 1), 0, word) or search(
+        lineFile, startCoordX1, (1, 1), 0, word[::-1]
+    )
+    diagonal2 = search(lineFile, startCoordX2, (-1, 1), 0, word) or search(
+        lineFile, startCoordX2, (-1, 1), 0, word[::-1]
+    )
+    return diagonal and diagonal2
+
+
+totalEx2 = 0
+for y, line in enumerate(lineFile):
+    for x, letter in enumerate(line):
+        if letter == "A":
+            totalEx2 += foundX(lineFile, (x, y), 1, "MAS")
+print(totalEx2)
