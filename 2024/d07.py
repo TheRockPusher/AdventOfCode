4a1a5036e1475486b44b15c1ddde4df139c31a89
TheRockPusher <github@orvit.simplelogin.com>
2024-12-07 21:40:10 +0000

    d7 EX2

--- 2024/d07.py
+++ 2024/d07.py
@@ -1,3 +1,6 @@
+from operator import add, mul
+from typing import Callable
+
 with open("2024/inputs/d07input.txt") as f:
     lineFile = [
         (int(line.split(":")[0]), list(map(int, line.split(":")[1].strip().split(" "))))
@@ -5,18 +8,36 @@ with open("2024/inputs/d07input.txt") as f:
     ]
 
 
-def multiAdd(value: int, operands: list[int]) -> bool:
+def multiAdd(
+    value: int, operands: list[int], operators: list[Callable[[int, int], int]]
+) -> bool:
     if len(operands) == 2:
-        return (value == operands[0] + operands[1]) or (
-            value == operands[0] * operands[1]
+        return any(
+            [value == operator(operands[0], operands[1]) for operator in operators]
         )
     if len(operands) == 1:
         return value == operands[0]
     if operands[0] >= value:
         return False
-    return multiAdd(value, [operands[0] + operands[1]] + operands[2:]) or multiAdd(
-        value, [operands[0] * operands[1]] + operands[2:]
+    return any(
+        [
+            multiAdd(
+                value, [operator(operands[0], operands[1])] + operands[2:], operators
+            )
+            for operator in operators
+        ]
     )
 
 
-print(sum([k for k, v in lineFile if multiAdd(k, v)]))
+print(
+    f"Exercise 1 -> {sum([line[0] for line in lineFile if multiAdd(line[0], line[1],[add,mul])])}"
+)
+
+
+def concatNumbers(n1: int, n2: int) -> int:
+    return int(str(n1) + str(n2))
+
+
+print(
+    f"Exercise 2 -> {sum([line[0] for line in lineFile if multiAdd(line[0], line[1],[add,mul,concatNumbers])])}"
+)
