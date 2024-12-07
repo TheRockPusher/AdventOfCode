6c91012c693b02e9f8ae32d4951d5275dfadbf94
TheRockPusher <github@orvit.simplelogin.com>
2024-12-07 19:16:59 +0000

    d06 EX2

--- 2024/d06.py
+++ 2024/d06.py
@@ -1,9 +1,15 @@
 from __future__ import annotations
 
+from copy import deepcopy
 from dataclasses import dataclass
 
 with open("2024/inputs/d06input.txt") as f:
     lineFile = f.readlines()
+
+
+def rebuild_line(linefile, x, y, letter: str):
+    linefile[y] = linefile[y][:x] + letter + linefile[y][x + 1 :]
+    return linefile
 
 
 @dataclass
@@ -13,11 +19,17 @@ class Guard:
     direction: tuple[int, int]
     distinctsteps: int = 0
 
-    def fullWalk(self, lineFile: list[str]):
+    def __post_init__(self):
+        self.positionhistory: set[tuple[int, int, tuple[int, int]]] = {
+            (self.x, self.y, self.direction)
+        }
+
+    def fullWalk(self, lineFile: list[str]) -> int:
         self.distinctsteps += 1
         lineFile[self.y] = (
-            lineFile[self.y][: self.x] + "X" + lineFile[self.y][self.x + 1 :]
+            lineFile[self.y][: self.x] + "S" + lineFile[self.y][self.x + 1 :]
         )
+
         while True:
             nextstep = (self.x + self.direction[0], self.y + self.direction[1])
             if not (
@@ -27,18 +39,19 @@ class Guard:
             match lineFile[nextstep[1]][nextstep[0]]:
                 case "#":
                     self.direction = (-self.direction[1], self.direction[0])
-                case "X":
+                case pastposition if pastposition in ["X", "S"]:
                     self.x = nextstep[0]
                     self.y = nextstep[1]
                 case _:
                     self.x = nextstep[0]
                     self.y = nextstep[1]
                     self.distinctsteps += 1
-                    lineFile[self.y] = (
-                        lineFile[self.y][: self.x]
-                        + "X"
-                        + lineFile[self.y][self.x + 1 :]
-                    )
+                    lineFile = rebuild_line(lineFile, self.x, self.y, "X")
+            if self.positionhistory & {(self.x, self.y, self.direction)}:
+                return 0
+            self.positionhistory = self.positionhistory.union(
+                {(self.x, self.y, self.direction)}
+            )
 
 
 mapGuard = Guard(0, 0, (0, 0), -1)
@@ -50,5 +63,14 @@ for y, line in enumerate(lineFile):
         break
 if mapGuard.distinctsteps == -1:
     raise AttributeError("Guard wasn't found")
-
-print(f"Exercise 1 -> {mapGuard.fullWalk(lineFile)}")
+ex1Guard = deepcopy(mapGuard)
+ex1lineFile = deepcopy(lineFile)
+print(f"Exercise 1 -> {ex1Guard.fullWalk(ex1lineFile)}")
+uniquePositions = {
+    (x, y)
+    for x, y, dir in ex1Guard.positionhistory
+    if (x, y) != (mapGuard.x, mapGuard.y)
+}
+print(
+    f"Exercise 2 -> {[bool(deepcopy(mapGuard).fullWalk(rebuild_line(deepcopy(lineFile),x,y, "#"))) for x,y in uniquePositions].count(False)}"
+)
