1e8e0629d659e55ca04351052bac98e9276c4723
TheRockPusher <github@orvit.simplelogin.com>
2024-12-09 15:45:28 +0000

    d9 Ex1 test pass

---
+++ 2024/d09.py
@@ -0,0 +1,61 @@
+from __future__ import annotations
+
+from copy import deepcopy
+from dataclasses import dataclass
+
+
+@dataclass
+class memoryBlock:
+    ID: int
+    length: int
+
+
+with open("2024/inputs/d09input.txt") as f:
+    listLines: list[memoryBlock] = []
+    free = False
+    blockID = 0
+    for letter in f.readlines()[0]:
+        if free:
+            listLines.append(memoryBlock(-1, int(letter)))
+        else:
+            listLines.append(memoryBlock(blockID, int(letter)))
+            blockID += 1
+        free = not free
+
+
+def compaction(memoryListOriginal: list[memoryBlock]) -> list[memoryBlock]:
+    memoryList: list[memoryBlock] = deepcopy(memoryListOriginal)
+    i = 0
+    while i < len(memoryList):
+        block: memoryBlock = memoryList[i]
+        if block.ID == -1:
+            lastBlock: memoryBlock = memoryList[-1]
+            if lastBlock.length == block.length:
+                memoryList[i] = lastBlock
+                memoryList = memoryList[:-2]
+            elif lastBlock.length < block.length:
+                memoryList[i : i + 1] = [
+                    lastBlock,
+                    memoryBlock(-1, block.length - lastBlock.length),
+                ]
+                memoryList = memoryList[:-2]
+            else:
+                memoryList[i] = memoryBlock(lastBlock.ID, block.length)
+                memoryList[-1] = memoryBlock(
+                    lastBlock.ID, lastBlock.length - block.length
+                )
+        else:
+            i += 1
+    return memoryList
+
+
+def checksumCalc(memoryList: list[memoryBlock]) -> int:
+    i = 0
+    res = 0
+    for block in memoryList:
+        res += sum([index * block.ID for index in range(i, i + block.length)])
+        i += block.length
+    return res
+
+
+print(f"Exercise 1 -> {checksumCalc(compaction(listLines))}")
