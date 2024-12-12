c1dc60e92445959a67cc156289c2a5e75e27fc52
TheRockPusher <github@orvit.simplelogin.com>
2024-12-12 12:30:52 +0000

    d12 test ex2

--- 2024/d12.py
+++ 2024/d12.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-with open("2024/inputs/d12input.txt") as f:
+with open("2024/inputs/d12test.txt") as f:
     farmingPlot = [plotLine.strip() for plotLine in f.readlines()]
 
 
@@ -19,11 +19,27 @@ class Coordinate:
 class Region:
     area: int
     perimeter: int
+    # tuple[Coordinate, bool] bool= true if horizontal
+    perimeterCoordinates: set[tuple[Coordinate,bool]]
 
     def __add__(self, region2) -> Region:
         return Region(
-            area=self.area + region2.area, perimeter=self.perimeter + region2.perimeter
+            area=self.area + region2.area, perimeter=self.perimeter + region2.perimeter, perimeterCoordinates=self.perimeterCoordinates.union(region2.perimeterCoordinates)
         )
+    
+    def sides(self):
+        currentSides:list[tuple[set[Coordinate],bool]] = []
+        directions = [Coordinate(1,0),Coordinate(-1,0), Coordinate(0,1), Coordinate(0,-1)]
+        for coord, horizontal in self.perimeterCoordinates:
+            coordLookFor = {(coord+direction,horizontal) for direction in directions}
+            intersectedSets:tuple[set[Coordinate],bool] = ({coord},horizontal)
+            for side in currentSides:
+                if coordLookFor&side:
+                    currentSides.remove(side)
+                    intersectedSets = intersectedSets.union((side,horizontal))
+            currentSides.append(intersectedSets)
+        return len(currentSides)
+
 
 
 def regionCounter(
@@ -32,7 +48,7 @@ def regionCounter(
     seenPlots.add(plotCoordinate)
     maxX = len(plotList[0])
     maxY = len(plotList)
-    region = Region(1, 0)
+    region = Region(1, 0,set())
     for direction in {
         Coordinate(-1, 0),
         Coordinate(1, 0),
@@ -53,6 +69,7 @@ def regionCounter(
                 region += newRegion
             else:
                 region.perimeter += 1
+                region.perimeterCoordinates.add((newCoordinate,bool(direction.x)))
 
     return region, seenPlots
 
@@ -66,7 +83,7 @@ def regionAdder(plotList: list[str]):
                 newRegion, newSeen = regionCounter(plotList, Coordinate(x, y), set())
                 totalCost += newRegion.area * newRegion.perimeter
                 seenPlots = seenPlots.union(newSeen)
-                # print(f"{plotList[y][x]} ->area{newRegion.area}   perimeter{newRegion.perimeter}")
+                print(f"{plotList[y][x]} ->area{newRegion.area}   perimeter{newRegion.perimeter}  sides{newRegion.sides()}")
     return totalCost
 
 
