db41637af96d3fb4302a9a599623f2e61c388107
TheRockPusher <github@orvit.simplelogin.com>
2024-12-12 14:25:25 +0000

    d12 ex2

--- 2024/d12.py
+++ 2024/d12.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 
-with open("2024/inputs/d12test.txt") as f:
+with open("2024/inputs/d12input.txt") as f:
     farmingPlot = [plotLine.strip() for plotLine in f.readlines()]
 
 
@@ -20,28 +20,38 @@ class Region:
     area: int
     perimeter: int
     # tuple[Coordinate, bool] bool= true if horizontal
-    perimeterCoordinates: set[tuple[Coordinate,bool]]
+    perimeterCoordinates: set[tuple[Coordinate, Coordinate]]
 
     def __add__(self, region2) -> Region:
         return Region(
-            area=self.area + region2.area, perimeter=self.perimeter + region2.perimeter, perimeterCoordinates=self.perimeterCoordinates.union(region2.perimeterCoordinates)
+            area=self.area + region2.area,
+            perimeter=self.perimeter + region2.perimeter,
+            perimeterCoordinates=self.perimeterCoordinates.union(
+                region2.perimeterCoordinates
+            ),
         )
-    
+
     def sides(self):
-        currentSides:list[set[tuple[Coordinate,bool]]] = []
-        directions = [Coordinate(1,0),Coordinate(-1,0), Coordinate(0,1), Coordinate(0,-1)]
-        for coord, horizontal in self.perimeterCoordinates:
-            intersectedSide:set[tuple[Coordinate,bool]] = {(coord,horizontal)}
+        currentSides: list[set[tuple[Coordinate, Coordinate]]] = []
+        directions = [
+            Coordinate(1, 0),
+            Coordinate(-1, 0),
+            Coordinate(0, 1),
+            Coordinate(0, -1),
+        ]
+        for coord, perimeterDirection in self.perimeterCoordinates:
+            intersectedSide: set[tuple[Coordinate, Coordinate]] = {
+                (coord, perimeterDirection)
+            }
             for direction in directions:
                 for side in currentSides:
-                    if (coord+direction,horizontal) in side:
-                        intersectedSide=intersectedSide.union(side)
-                        currentSides.remove(side)           
+                    if (coord + direction, perimeterDirection) in side:
+                        intersectedSide = intersectedSide.union(side)
+                        currentSides.remove(side)
             currentSides.append(intersectedSide)
-        for side in currentSides:
-            print(side)
+        # for side in currentSides:
+        #     print(side)
         return len(currentSides)
-
 
 
 def regionCounter(
@@ -50,7 +60,7 @@ def regionCounter(
     seenPlots.add(plotCoordinate)
     maxX = len(plotList[0])
     maxY = len(plotList)
-    region = Region(1, 0,set())
+    region = Region(1, 0, set())
     for direction in {
         Coordinate(-1, 0),
         Coordinate(1, 0),
@@ -71,23 +81,25 @@ def regionCounter(
                 region += newRegion
             else:
                 region.perimeter += 1
-                region.perimeterCoordinates.add((newCoordinate,bool(direction.y)))
+                region.perimeterCoordinates.add((newCoordinate, direction))
 
     return region, seenPlots
 
 
 def regionAdder(plotList: list[str]):
     totalCost = 0
+    totalCostPerSide = 0
     seenPlots: set[Coordinate] = set()
     for y, line in enumerate(plotList):
         for x, letter in enumerate(line):
             if Coordinate(x, y) not in seenPlots:
                 newRegion, newSeen = regionCounter(plotList, Coordinate(x, y), set())
                 totalCost += newRegion.area * newRegion.perimeter
+                totalCostPerSide += newRegion.area * newRegion.sides()
                 seenPlots = seenPlots.union(newSeen)
-                print(f"{plotList[y][x]} ->area{newRegion.area}   perimeter{newRegion.perimeter}  sides{newRegion.sides()}")
-    return totalCost
+    return totalCost, totalCostPerSide
 
 
-totalCost = regionAdder(farmingPlot)
+totalCost, totalCostPerSide = regionAdder(farmingPlot)
 print(f"Exercise 1 -> {totalCost}")
+print(f"Exercise 2 -> {totalCostPerSide}")
