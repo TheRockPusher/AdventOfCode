a3e4696f5406668bedea7b0a526852137c786095
TheRockPusher <github@orvit.simplelogin.com>
2024-12-16 16:13:59 +0000

    d15 Ex2 lint

--- 2024/d15.py
+++ 2024/d15.py
@@ -2,126 +2,147 @@ from copy import deepcopy
 
 with open("2024/inputs/d15input.txt") as f:
     text: list[str] = f.readlines()
-    originalGraph: list[str]= [t.strip() for t in text[:50]]
+    originalGraph: list[str] = [t.strip() for t in text[:50]]
     movement: str = "".join([t.strip() for t in text[51:]])
 
 
-def translationCoord(letter:str)->tuple[int,int]:
+def translationCoord(letter: str) -> tuple[int, int]:
     match letter:
         case "v":
-            return (0,1)
+            return (0, 1)
         case ">":
-            return (1,0)
+            return (1, 0)
         case "^":
-            return (0,-1)
+            return (0, -1)
         case "<":
-            return (-1,0)
-        case _ :
+            return (-1, 0)
+        case _:
             print(letter)
-            raise(ValueError)
-        
-i=True
-j=0
-def push(graph:list[str],coordToPush:tuple[int,int], movement:str)->bool: 
-    global i
-    global j
-    j+=1
-    if i and 370<j<400:
-        print(j)
-        for r in graph:
-            print(r)
-            i=False
-        print(movement)
-    movementCoordinates=translationCoord(movement)    
-    nextCoord=(coordToPush[0]+movementCoordinates[0], coordToPush[1]+movementCoordinates[1])
+            raise (ValueError)
+
+
+def push(graph: list[str], coordToPush: tuple[int, int], movement: str) -> bool:
+    movementCoordinates = translationCoord(movement)
+    nextCoord = (
+        coordToPush[0] + movementCoordinates[0],
+        coordToPush[1] + movementCoordinates[1],
+    )
     match graph[nextCoord[1]][nextCoord[0]]:
         case "#":
             return False
         case ".":
-            graph[nextCoord[1]] = graph[nextCoord[1]][:nextCoord[0]]+graph[coordToPush[1]][coordToPush[0]]+graph[nextCoord[1]][nextCoord[0]+1:]
-            graph[coordToPush[1]] = graph[coordToPush[1]][:coordToPush[0]]+ "."+graph[coordToPush[1]][coordToPush[0]+1:]
+            graph[nextCoord[1]] = (
+                graph[nextCoord[1]][: nextCoord[0]]
+                + graph[coordToPush[1]][coordToPush[0]]
+                + graph[nextCoord[1]][nextCoord[0] + 1 :]
+            )
+            graph[coordToPush[1]] = (
+                graph[coordToPush[1]][: coordToPush[0]]
+                + "."
+                + graph[coordToPush[1]][coordToPush[0] + 1 :]
+            )
             return True
         case "O":
-            if push(graph,nextCoord, movement):
-                graph[nextCoord[1]] = graph[nextCoord[1]][:nextCoord[0]]+graph[coordToPush[1]][coordToPush[0]]+graph[nextCoord[1]][nextCoord[0]+1:]
-                graph[coordToPush[1]] = graph[coordToPush[1]][:coordToPush[0]]+ "."+graph[coordToPush[1]][coordToPush[0]+1:]
+            if push(graph, nextCoord, movement):
+                graph[nextCoord[1]] = (
+                    graph[nextCoord[1]][: nextCoord[0]]
+                    + graph[coordToPush[1]][coordToPush[0]]
+                    + graph[nextCoord[1]][nextCoord[0] + 1 :]
+                )
+                graph[coordToPush[1]] = (
+                    graph[coordToPush[1]][: coordToPush[0]]
+                    + "."
+                    + graph[coordToPush[1]][coordToPush[0] + 1 :]
+                )
                 return True
             else:
                 return False
-        case w if w in ["[","]"]:
-            if movementCoordinates in [(1,0),(-1,0)]:
-                if push(graph,nextCoord, movement):
-                    graph[nextCoord[1]] = graph[nextCoord[1]][:nextCoord[0]]+graph[coordToPush[1]][coordToPush[0]]+graph[nextCoord[1]][nextCoord[0]+1:]
-                    graph[coordToPush[1]] = graph[coordToPush[1]][:coordToPush[0]]+ "."+graph[coordToPush[1]][coordToPush[0]+1:]
+        case w if w in ["[", "]"]:
+            if movementCoordinates in [(1, 0), (-1, 0)]:
+                if push(graph, nextCoord, movement):
+                    graph[nextCoord[1]] = (
+                        graph[nextCoord[1]][: nextCoord[0]]
+                        + graph[coordToPush[1]][coordToPush[0]]
+                        + graph[nextCoord[1]][nextCoord[0] + 1 :]
+                    )
+                    graph[coordToPush[1]] = (
+                        graph[coordToPush[1]][: coordToPush[0]]
+                        + "."
+                        + graph[coordToPush[1]][coordToPush[0] + 1 :]
+                    )
                     return True
                 else:
                     return False
             else:
                 tempGraph = deepcopy(graph)
-                otherNextCoord = (nextCoord[0]+1,nextCoord[1]) if w =="[" else (nextCoord[0]-1,nextCoord[1])
-                otherCoord = (coordToPush[0]+1,coordToPush[1]) if w =="[" else (coordToPush[0]-1,coordToPush[1])
-                if push(graph,nextCoord,movement) and push(graph,otherNextCoord,movement):
-                    graph[nextCoord[1]] = graph[nextCoord[1]][:nextCoord[0]]+graph[coordToPush[1]][coordToPush[0]]+graph[nextCoord[1]][nextCoord[0]+1:]
-                    graph[coordToPush[1]] = graph[coordToPush[1]][:coordToPush[0]]+ "."+graph[coordToPush[1]][coordToPush[0]+1:]
+                otherNextCoord = (
+                    (nextCoord[0] + 1, nextCoord[1])
+                    if w == "["
+                    else (nextCoord[0] - 1, nextCoord[1])
+                )
+                if push(graph, nextCoord, movement) and push(
+                    graph, otherNextCoord, movement
+                ):
+                    graph[nextCoord[1]] = (
+                        graph[nextCoord[1]][: nextCoord[0]]
+                        + graph[coordToPush[1]][coordToPush[0]]
+                        + graph[nextCoord[1]][nextCoord[0] + 1 :]
+                    )
+                    graph[coordToPush[1]] = (
+                        graph[coordToPush[1]][: coordToPush[0]]
+                        + "."
+                        + graph[coordToPush[1]][coordToPush[0] + 1 :]
+                    )
                     return True
                 else:
-                    graph[:]=tempGraph
+                    graph[:] = tempGraph
                     return False
-                
+
         case _:
             print(graph[nextCoord[1]][nextCoord[0]])
             raise (TypeError)
 
-mapGraph = deepcopy(originalGraph)
-x,y = (-1,-1)      
-for y,line in enumerate(mapGraph):
-    try:
-        x=line.index("@")
-        break
-    except ValueError:
-        pass
-# print(movement)
-for singleMovement in movement:
-    i=False
-    moved=push(mapGraph,(x,y),singleMovement)
-    if moved:
-        movementX, movementY= translationCoord(singleMovement)
-        x=x+movementX
-        y=y+movementY
-print(f"Exercise 1 -> {sum([100*y+x for y,row in enumerate(mapGraph) for x,char in enumerate(row) if char =='O'])}")
 
-mapGraphEx2:list[str]= []
+def cycle(graph, movement):
+    x, y = (-1, -1)
+    for y, line in enumerate(graph):
+        try:
+            x = line.index("@")
+            break
+        except ValueError:
+            pass
+    # print(movement)
+    for singleMovement in movement:
+        moved = push(graph, (x, y), singleMovement)
+        if moved:
+            movementX, movementY = translationCoord(singleMovement)
+            x = x + movementX
+            y = y + movementY
+    return graph
+
+
+mapGraph = deepcopy(originalGraph)
+print(
+    f"Exercise 1 -> {sum([100*y+x for y,row in enumerate(cycle(mapGraph,movement)) for x,char in enumerate(row) if char =='O'])}"
+)
+
+mapGraphEx2: list[str] = []
 for row in originalGraph:
     stringToAppend = ""
     for char in row:
         match char:
             case "#":
-                stringToAppend=stringToAppend+"##"
+                stringToAppend = stringToAppend + "##"
             case ".":
-                stringToAppend=stringToAppend+".."
+                stringToAppend = stringToAppend + ".."
             case "O":
-                stringToAppend=stringToAppend+"[]"
+                stringToAppend = stringToAppend + "[]"
             case "@":
-                stringToAppend=stringToAppend+"@."
+                stringToAppend = stringToAppend + "@."
             case _:
                 raise ValueError
     mapGraphEx2.append(stringToAppend)
 
-x,y = (-1,-1)      
-for y,line in enumerate(mapGraphEx2):
-    j=0
-    try:
-        x=line.index("@")
-        break
-    except ValueError:
-        pass
-for singleMovement in movement:
-    i=True
-    moved=push(mapGraphEx2,(x,y),singleMovement)
-    if moved:
-        movementX, movementY= translationCoord(singleMovement)
-        x=x+movementX
-        y=y+movementY
-print(f"Exercise 2 -> {sum([100*y+x for y,row in enumerate(mapGraphEx2) for x,char in enumerate(row) if char =='['])}")
-for row in mapGraphEx2:
-    print(row)
+print(
+    f"Exercise 2 -> {sum([100*y+x for y,row in enumerate(cycle(mapGraphEx2,movement)) for x,char in enumerate(row) if char =='['])}"
+)
