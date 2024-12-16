eabed7e6ed9a3ddce3b8e03351bed6dc50351712
TheRockPusher <github@orvit.simplelogin.com>
2024-12-16 23:51:06 +0000

    d16 Ex2 test

--- 2024/d16.py
+++ 2024/d16.py
@@ -1,6 +1,6 @@
-from queue import PriorityQueue
+from queue import PriorityQueue, Queue
 
-with open("2024/inputs/d16input.txt") as f:
+with open("2024/inputs/d16test.txt") as f:
     maze = [t.strip() for t in f.readlines()]
 
 end = (len(maze[0]) - 2, 1)
@@ -30,5 +30,24 @@ def dijkstra(maze: list[str], start: tuple[int, int]):
                     placesToVisit.put((neighbourScore, (x, y, coord[0], coord[1])))
     return mapDistance
 
+Ex1 = dijkstra(maze, start)
+print(f"Exercise 1 -> {Ex1[end]}")
 
-print(dijkstra(maze, start)[end])
+def pathSearcher(distanceMap: dict[tuple[int,int],int], end: tuple[int,int]):
+    placesToSit = Queue()
+    placesToSit.put((end[0],end[1]))
+    seat={end}
+    while not placesToSit.empty():
+        baseX,baseY=placesToSit.get()
+        for coord in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            x=baseX+coord[0]
+            y = baseY + coord[1]
+            if maze[y][x] != "#":
+                if distanceMap[(baseX,baseY)]-distanceMap[(x,y)]in[1,1001]:
+                    placesToSit.put((x,y))
+                    seat.add((x,y))
+    return seat
+
+print(f"Exercise 1 -> {len(pathSearcher(Ex1,end))}")
+
+
