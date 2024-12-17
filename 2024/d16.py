ceada9f44662570a0b3b14fb5ae56272330ddc20
TheRockPusher <github@orvit.simplelogin.com>
2024-12-17 10:43:38 +0000

    d16 Ex2

--- 2024/d16.py
+++ 2024/d16.py
@@ -1,6 +1,6 @@
 from queue import PriorityQueue, Queue
 
-with open("2024/inputs/d16test.txt") as f:
+with open("2024/inputs/d16input.txt") as f:
     maze = [t.strip() for t in f.readlines()]
 
 end = (len(maze[0]) - 2, 1)
@@ -30,24 +30,44 @@ def dijkstra(maze: list[str], start: tuple[int, int]):
                     placesToVisit.put((neighbourScore, (x, y, coord[0], coord[1])))
     return mapDistance
 
+
 Ex1 = dijkstra(maze, start)
 print(f"Exercise 1 -> {Ex1[end]}")
 
-def pathSearcher(distanceMap: dict[tuple[int,int],int], end: tuple[int,int]):
+
+def pathSearcher(distanceMap: dict[tuple[int, int], int], end: tuple[int, int]):
     placesToSit = Queue()
-    placesToSit.put((end[0],end[1]))
-    seat={end}
+    placesToSit.put((end[0], end[1], 1, 1))
+    seat: set[tuple[int, int]] = {end}
+    Turn = 0
     while not placesToSit.empty():
-        baseX,baseY=placesToSit.get()
+        if Turn:
+            Turn -= 1
+        baseX, baseY, _, _ = placesToSit.get()
         for coord in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
-            x=baseX+coord[0]
+            x = baseX + coord[0]
             y = baseY + coord[1]
             if maze[y][x] != "#":
-                if distanceMap[(baseX,baseY)]-distanceMap[(x,y)]in[1,1001]:
-                    placesToSit.put((x,y))
-                    seat.add((x,y))
+                distance = distanceMap[(baseX, baseY)] - distanceMap[(x, y)]
+                if distance == 1:
+                    placesToSit.put((x, y, coord[0], coord[1]))
+                    seat.add((x, y))
+                elif distance == 1001:
+                    Turn = 2
+                    placesToSit.put((x, y, coord[0], coord[1]))
+                    seat.add((x, y))
+                elif distance == -999 and Turn:
+                    placesToSit.put((x, y, coord[0], coord[1]))
+                    seat.add((x, y))
     return seat
 
-print(f"Exercise 1 -> {len(pathSearcher(Ex1,end))}")
 
+# endMaze = dijkstra(maze,end)
+# seats = []
+# for coordinate,score in endMaze.items():
+#     if Ex1[end]-score==Ex1[coordinate]:
+#         seats.append(coordinate)
 
+# print(seats)
+ex2 = pathSearcher(Ex1, end)
+print(f"Exercise 2 -> {len(ex2)}")
