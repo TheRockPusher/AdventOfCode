5193f6f33813cdb2dc69d59f8c6a5c3c2f6caa78
TheRockPusher <github@orvit.simplelogin.com>
2024-12-19 10:58:08 +0000

    d19 EX2

--- 2024/d19.py
+++ 2024/d19.py
@@ -1,3 +1,5 @@
+from functools import lru_cache
+
 with open("2024/inputs/d19input.txt") as f:
     allText = f.readlines()
     towelPatterns = [pattern.strip() for pattern in allText[0].split(",")]
@@ -16,8 +18,27 @@ def combinationFitter(combination: str, patterns: list[str]) -> bool:
     return False
 
 
-res = 0
+ex1 = 0
 for combination in combinations:
-    res += combinationFitter(combination, towelPatterns)
+    ex1 += combinationFitter(combination, towelPatterns)
 
-print(f"Exercise 1 -> {res}")
+print(f"Exercise 1 -> {ex1}")
+
+
+@lru_cache
+def combinationAdder(combination: str, patterns: frozenset[str]) -> int:
+    res = 0
+    if not combination:
+        return 1
+    for pattern in patterns:
+        patternLen = len(pattern)
+        if combination[:patternLen] == pattern:
+            res += combinationAdder(combination[patternLen:], patterns)
+    return res
+
+
+ex2 = 0
+for combination in combinations:
+    ex2 += combinationAdder(combination, frozenset(towelPatterns))
+
+print(f"Exercise 2 -> {ex2}")
